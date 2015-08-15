f33c4f538378bcbe666c9c65af3d78179fc2595b
iBener <ibrahimbener@gmail.com>
2015-08-15 12:40:15 +0300

    adding code

--- ConvertSrtToPlainText/ConvertSrtToPlainText/Program.cs
+++ ConvertSrtToPlainText/ConvertSrtToPlainText/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using IO = System.IO;
 
 namespace ConvertSrtToPlainText
 {
@@ -9,6 +10,34 @@ namespace ConvertSrtToPlainText
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                return;
+            }
+            var lines = IO.File.ReadAllLines(args[0]);
+            var line = String.Empty;
+            var result = new List<string>();
+            var stepLine = 2;
+            foreach (var l in lines)
+            {
+                if (stepLine > 0)
+                {
+                    if (!String.IsNullOrEmpty(line))
+                    {
+                        result.Add(line.Trim());
+                        line = String.Empty;
+                    }
+                    stepLine -= 1;
+                    continue;
+                }
+                if (String.IsNullOrWhiteSpace(l))
+                {
+                    stepLine = 2;
+                    continue;
+                }
+                line += l + " ";
+            }
+            IO.File.WriteAllLines(args[1], result.ToArray());
         }
     }
 }
