1b7e926a2b5086a0e0f1454e12f0c62ef7c100fa
iBener <ibrahimbener@gmail.com>
2015-10-31 13:26:50 +0300

    data visualizer project initial commit

---
+++ Bener.DataVisualizer/Bener.DataVisualizer/DictionaryDialogDebuggerVisualizer.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+[assembly: System.Diagnostics.DebuggerVisualizer(
+    typeof(Bener.DataVisualizer.DictionaryDialogDebuggerVisualizer),
+    typeof(Bener.DataVisualizer.DictionaryVisualizerObjectSource),
+    Target = typeof(Dictionary<,>),
+    Description = "Bener Dictionary Visualizer")]
+
+namespace Bener.DataVisualizer
+{
+
+    public class DictionaryDialogDebuggerVisualizer : DialogDebuggerVisualizer
+    {
+
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            try
+            {
+                var sr = new StreamReader(objectProvider.GetData());
+                var obj = sr.ReadToEnd();
+                MessageBox.Show(obj.Substring(0, 500));
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+
+    public class DictionaryVisualizerObjectSource : VisualizerObjectSource
+    {
+
+        public override void GetData(object target, Stream outgoingData)
+        {
+            var dict = target as IDictionary;
+            if (dict != null)
+            {
+                var sonuc = new System.Xml.Linq.XElement("Result");
+                foreach (var key in dict.Keys)
+                {
+                    var data = new System.Xml.Linq.XElement("Data");
+                    data.SetAttributeValue("key", key);
+                    var obj = dict[key];
+                    foreach (var prp in obj.GetType().GetProperties())
+                    {
+                        var objData = prp.GetValue(obj, null);
+                        if (objData != null)
+                        {
+                            data.SetAttributeValue(prp.Name, objData.ToString());
+                        }
+                    }
+                    sonuc.Add(data);
+                }
+                var writer = new StreamWriter(outgoingData);
+                writer.Write(sonuc.ToString());
+                writer.Flush();
+            }
+        }
+
+    }
+}
---
+++ Bener.DataVisualizer/Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bener.DataVisualizer;
+using System.Windows.Forms;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bener.DataVisualizer.ControlVisualizer.TestShowVisualizer(new Control("Hello World!"));
+
+            var dic = new Dictionary<int, Person>();
+            dic.Add(3, new Person() { Id = 3, Name = "Hacı Hüsrev" });
+            dic.Add(5, new Person() { Id = 5, Name = "Naci" });
+            Tester.TestShowVisualizer(dic);
+        }
+    }
+
+
+    //[Serializable]
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    struct KartBilgisi
+    {
+        public System.Data.DataSet DataSet { get; set; }
+        public System.Data.DataRow Row { get; set; }
+    }
+}
