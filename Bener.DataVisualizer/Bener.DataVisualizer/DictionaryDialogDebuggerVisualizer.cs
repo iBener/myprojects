16420a887df3ac223b8d4736e761d4bc8fef2039
iBener <ibrahimbener@gmail.com>
2015-11-02 00:39:30 +0300

    second

--- Bener.DataVisualizer/Bener.DataVisualizer/DictionaryDialogDebuggerVisualizer.cs
+++ Bener.DataVisualizer/Bener.DataVisualizer/DictionaryDialogDebuggerVisualizer.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.DebuggerVisualizers;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+using System.Data;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using IO = System.IO;
 
 [assembly: System.Diagnostics.DebuggerVisualizer(
     typeof(Bener.DataVisualizer.DictionaryDialogDebuggerVisualizer),
@@ -20,9 +22,13 @@ namespace Bener.DataVisualizer
         {
             try
             {
-                var sr = new StreamReader(objectProvider.GetData());
-                var obj = sr.ReadToEnd();
-                MessageBox.Show(obj.Substring(0, 500));
+                var data = new XElement("Result");
+                using (var sr = new IO.StreamReader(objectProvider.GetData()))
+                {
+                    data = XElement.Parse(sr.ReadToEnd());
+                }
+                var dt = new DataTable();
+                
             }
             catch (System.Exception ex)
             {
@@ -33,18 +39,35 @@ namespace Bener.DataVisualizer
 
     public class DictionaryVisualizerObjectSource : VisualizerObjectSource
     {
-
-        public override void GetData(object target, Stream outgoingData)
+        public override void GetData(object target, IO.Stream outgoingData)
         {
             var dict = target as IDictionary;
             if (dict != null)
             {
-                var sonuc = new System.Xml.Linq.XElement("Result");
+                var sonuc = new XElement("Data");
+                var t = new XElement("TypeInfo");
+                var types = dict.GetType().GetGenericArguments();
+                t.SetAttributeValue("KeyType", types[0].FullName);
+                t.SetAttributeValue("ValueType", types[1].FullName);
+                sonuc.Add(t);
+                XElement prpt = null;
+                var items = new XElement("Items");
                 foreach (var key in dict.Keys)
                 {
-                    var data = new System.Xml.Linq.XElement("Data");
+                    var data = new XElement("Item");
                     data.SetAttributeValue("key", key);
                     var obj = dict[key];
+                    if (prpt == null)
+                    {
+                        prpt = new XElement("Properties");
+                        foreach (var prp in obj.GetType().GetProperties())
+                        {
+                            var p = new XElement("Property");
+                            p.SetAttributeValue("Name", prp.Name);
+                            p.SetAttributeValue("Type", prp.DeclaringType.FullName);
+                            prpt.Add(p);
+                        }
+                    }
                     foreach (var prp in obj.GetType().GetProperties())
                     {
                         var objData = prp.GetValue(obj, null);
@@ -53,13 +76,14 @@ namespace Bener.DataVisualizer
                             data.SetAttributeValue(prp.Name, objData.ToString());
                         }
                     }
-                    sonuc.Add(data);
+                    items.Add(data);
                 }
-                var writer = new StreamWriter(outgoingData);
+                sonuc.Add(prpt);
+                sonuc.Add(items);
+                var writer = new IO.StreamWriter(outgoingData);
                 writer.Write(sonuc.ToString());
                 writer.Flush();
             }
         }
-
     }
 }
