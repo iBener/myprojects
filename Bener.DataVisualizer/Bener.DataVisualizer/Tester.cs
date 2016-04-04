0ef2cc6c0c1fe13db0788cdeeba907cd411faca9
iBener <ibrahimbener@gmail.com>
2016-04-04 13:31:29 +0300

    Revert "taşıma"
    This reverts commit ae18a76dbf4f23b99350f45cd5dbdaa833f598c1.

---
+++ Bener.DataVisualizer/Bener.DataVisualizer/Tester.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bener.DataVisualizer
+{
+    public class Tester
+    {
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            VisualizerDevelopmentHost visualizerHost = 
+                new VisualizerDevelopmentHost(objectToVisualize, 
+                    typeof(DictionaryDialogDebuggerVisualizer), typeof(DictionaryVisualizerObjectSource));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
